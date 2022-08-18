9491c843e67c12bd0403fc84862ee878b77eee2a
bdauria1 <103322615+bdauria1@users.noreply.github.com>
2022-08-18 00:25:37 -0400

    debugged some more but can't seem to figure out why the snake is going so fast

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.querySelector('.start')
     const width = 10
     let currentIndex = 0
-    let appleIndex = 0
+    let foodIndex = 0
 
     let currentSnake = [2,1,0]
 
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function startGame(){
         currentSnake.forEach(index => squares[index].classList.remove('snake'))
-        squares[appleIndex].classList.remove('apple')
+        squares[foodIndex].classList.remove('food')
         clearInterval(interval)
         score = 0
         direction = 1
@@ -36,25 +36,25 @@ document.addEventListener('DOMContentLoaded', () => {
         squares[tail].classList.remove('snake')
         currentSnake.unshift(currentSnake[0] + direction)
     
-        if(squares[currentSnake[0]].classList.contains('apple')){
-            squares.currentSnake[0].remove('apple')
+        if(squares[currentSnake[0]].classList.contains('food')){
+            squares.currentSnake[0].remove('food')
             squares[tail].classList.add('snake')
             currentSnake.push(tail)
-            randomApple()
+            randomfood()
             score++
             scoreDisplay.textContent = score
             clearInterval(interval)
-            intervalTime = intervalTime * speed
+            intervalTime = intervalTime + speed
             interval = setInterval(moveOutcomes, intervalTime)
         }
         squares[currentSnake[0]].classList.add('snake')
     }
 
-   function randomApple(){
+   function randomfood(){
     do{
-        appleIndex = Math.floor(Math.random() * square.length)
-    }while(squares[appleIndex].classList.contains('snake'))
-    squares[appleIndex].classList.add('apple')
+        foodIndex = Math.floor(Math.random() * square.length)
+    }while(squares[foodIndex].classList.contains('snake'))
+    squares[foodIndex].classList.add('food')
    }
 
     function control(e) {
