d76ad6f33b65d76d4e223337a8278d3f2e12f5ca
bdauria1 <103322615+bdauria1@users.noreply.github.com>
2022-08-17 23:52:20 -0400

    added key controls and listeners

---
+++ app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const scoreDisplay = document.querySelector('span')
+    const startBtn = document.querySelector('.start')
+    const width = 10
+    let currentIndex = 0
+    let appleIndex = 0
+
+    let currentSnake = [2,1,0]
+
+    let direction = 1
+    let score = 0
+    let speed = .9
+    let intervalTime = 0
+    let interval = 0
+
+    function control(e) {
+        squares[currentIndex].classList.remove('snake')
+
+        if(e.KeyCode == 39){
+            direction = 1
+        }else if(e.KeyCode == 38){
+            direction = -width
+        }else if(e.KeyCode == 37){
+            direction = -1
+        }else if(e.KeyCode == 40){
+            direction = +width
+        }
+    }
+
+    document.addEventListener('keyup', control)
+})
